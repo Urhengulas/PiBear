24a2ca9449f0c76ad7fbe940ab4b8f549bfe321d
Urhengulas <joheihe@web.de>
2019-07-01 17:50:40 +0200

    Implement blinken()

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ nano = Nano()
 nano.reset_encoders()
 
 
+def blinken():
+    leds.set_led(c.LED_LEFT, c.RED)
+    sleep(0.5)
+    leds.set_led(c.LED_LEFT, c.OFF)
+    leds.set_led(c.LED_MID, c.YELLOW)
+    sleep(0.5)
+    leds.set_led(c.LED_MID, c.OFF)
+    leds.set_led(c.LED_RIGHT, c.GREEN)
+    sleep(0.5)
+    leds.set_led(c.LED_RIGHT, c.OFF)
+
+
 def stop_motors():
     nano.set_motors(0, 0)
 
@@ -22,6 +34,11 @@ def main():
 
     while True:
         nano.set_motors(speed, -speed)
+
+        blinken()
+
+        sleep(0.1)
+        stop_motors()
 
 
 if __name__ == "__main__":
