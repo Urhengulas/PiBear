ab4fe9b241bef2492fb9ce77da6523079a397e3f
Urhengulas <joheihe@web.de>
2019-07-01 17:35:14 +0200

    First running bot

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, I am Pibot")
+from time import sleep
+import logging
+
+from pibot import (
+    leds, buttons, constants as c
+)
+from pibot.nano import Nano
+
+leds.init_leds()
+buttons.init_buttons()
+
+nano = Nano()
+nano.reset_encoders()
+
+
+def stop_motors():
+    nano.set_motors(0, 0)
+
+
+def main():
+    speed = 20
+
+    while True:
+        nano.set_motors(speed, -speed)
+
+
+if __name__ == "__main__":
+    main()
